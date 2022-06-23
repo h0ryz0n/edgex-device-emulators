0a104a85f0598d6cf94261005ff1a1f06f9a3bb7
gck0 <20210112+gck0@users.noreply.github.com>
2022-06-23 19:04:10 +0200

    Create spawn-coap-client.sh

---
+++ spawn-coap-client.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+## coap client bot
+## sends data (int) to a coap uri
+
+COAPURI=coap://edgex/a1r/d1/int
+
+while [ True ];
+do
+  VAR=$((1 + RANDOM %10))
+  coap-client -v9 -m post -t 0 -e $VAR $COAPURI
+  sleep(5)
+done
